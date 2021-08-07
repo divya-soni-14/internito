0e92445b7c40bb07abfd9eea4737eaed3693d780
Mohd Sufiyan Ansari <addsufiyan@gmail.com>
2021-08-07 10:42:34 +0530

    Backend on DUMMY

---
+++ backend/experience/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Experience(models.Model):
+    name = models.CharField(max_length=80)
+    branch = models.CharField(max_length=255,blank=True)
+    cgpa_cutoff = models.FloatField(blank=True)
+    company = models.CharField(max_length=255,blank=False)
+    summary = models.TextField(blank=False)
+    period = models.CharField(max_length=255,blank=False)
+    ot_summary = models.TextField(blank = True)
+    ot_question1 = models.TextField(blank = True)
+    ot_question1_link = models.TextField(blank = True)
+    ot_question2 = models.TextField(blank = True)
+    ot_question2_link = models.TextField(blank = True)
+    ot_question3 = models.TextField(blank = True)
+    ot_question3_link = models.TextField(blank = True)
+    ot_question4 = models.TextField(blank = True)
+    ot_question4_link = models.TextField(blank = True)
+    round1_details = models.TextField(blank = True)
+    round2_details = models.TextField(blank = True)
+    round3_details = models.TextField(blank = True)
+    final_summary = models.TextField(blank = True)
+    
+    def __str__(self):
+        return self.name + '_' + self.company
+    
---
+++ backend/experience/views.py
@@ -0,0 +1,29 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from .models import *
+from .company import NAMES
+
+# Create your views here.
+def home(request):
+    responses = Experience.objects.all().order_by('-id')[:3]
+    return render(request, 'home.html',{'responses':responses})
+
+def login(request):
+    return render(request, 'login.html',{})
+
+def register(request):
+    return render(request, 'register.html',{})
+
+def company(request):
+    if request.method == 'POST':
+        company_name = request.POST['company']
+        responses = Experience.objects.filter(company__icontains=company_name)
+        message = 'Interview Experience of ' + company_name
+        return render(request, 'experience.html',{'responses':responses,'message':message})
+    elif request.method == 'GET':
+        return render(request, 'filter.html',{'companies':NAMES})
+
+def experience(request):
+    responses = Experience.objects.all().order_by('-id')
+    message = 'All Interview Experiences'
+    return render(request, 'experience.html',{'responses':responses,'message':message})
